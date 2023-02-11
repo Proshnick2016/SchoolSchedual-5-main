d8dc32198b70c2aec7f9fb57b4c8187b0f6e72f3
isOdin <44323275+Proshnick2016@users.noreply.github.com>
2023-02-11 07:41:56 +0000

    4DaysBeforeDeadLine

--- client/pages/Schedual.js
+++ client/pages/Schedual.js
@@ -8,21 +8,30 @@ export default function Schedual() {
     const [countLoops, setCountLoops] = useState(0);
 
     const [countLessons, setCountLessons] = useState(0); // номер урока
-    const [nextLessonHours, setNextLessonHours] = useState(22); //8
-    const [nextLessonMinutes, setNextLessonMinutes] = useState(57); // 0
+    const [nextLessonHours, setNextLessonHours] = useState(10); //8
+    const [nextLessonMinutes, setNextLessonMinutes] = useState(35); // 0
     const [nextLesson, setNextLesson] = useState(""); // ""
 
     async function setNewDataAboutLesson(lessonNumber) {
         const schedual = await AsyncStorage.getItem('Schedual');
-        // let weekDay = Алгоритм нахождения дня недели
-        // условие, если уроков на сегодня больше нет, то меняем данные об уроке на урок следующего дня (убрать условие из useEffet) 
-        setNextLesson(JSON.parse(schedual)['Понедельник'][lessonNumber]['lessonName']);
 
-        let lessonHours = JSON.parse(schedual)['Понедельник'][lessonNumber]['lessonTime'];
+        let date = new Date();
+        let weekDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        let day = 0;
+        if (date.getDay() === 0) {
+            day = 6;
+        } else {
+            day = date.getDay();
+        }
+
+        // условие, если уроков на сегодня больше нет, то меняем данные об уроке на урок следующего дня (убрать условие из useEffet) 
+        setNextLesson(JSON.parse(schedual)[weekDays[day]][lessonNumber]['lessonName']);
+
+        let lessonHours = JSON.parse(schedual)[weekDays[day]][lessonNumber]['lessonTime'];
         setNextLessonHours(parseInt(lessonHours[lessonHours.lenth - 5]) * 10 + parseInt(lessonHours[lessonHours.lenth - 4]));
 
-        let lessonMinutes = JSON.parse(schedual)['Понедельник'][lessonNumber]['lessonTime'];
-        setNextLessonMinutes(parseInt(lessonMinutes[lessonMinutes.lenth - 5]) * 10 + parseInt(lessonMinutes[lessonMinutes.lenth - 4]));
+        let lessonMinutes = JSON.parse(schedual)[weekDays[day]][lessonNumber]['lessonTime'];
+        setNextLessonMinutes(parseInt(lessonMinutes[lessonMinutes.lenth - 2]) * 10 + parseInt(lessonMinutes[lessonMinutes.lenth - 1]));
 
     }
 
@@ -32,6 +41,7 @@ export default function Schedual() {
             let todayTime = new Date(); // время в данный момент
             let hours = todayTime.getHours(); // часы
             let minutes = todayTime.getMinutes(); // минуты
+            // может надо будет ввести ещё и дату дня
 
             if ((hours + 3) % 24 === 7 && (minutes === 50 || minutes > 50)) {
                 setCountLessons(0);
@@ -46,8 +56,8 @@ export default function Schedual() {
                     // !трижды отправляется уведомление, затем больше ничего не идёт!
                 }
 
-                setCountLoops(countLoops + 1);
-            }
+            } // может, надо убрать else, чтобы прекратить отправку 3 сообщений подряд
+            setCountLoops(countLoops + 1);
         }, 10000); // 120000
     });
 
